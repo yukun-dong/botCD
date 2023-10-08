59b4a438453ce76205d501e0790ccbd9c5b9974b
yukun-dong <dongyukun1995@163.com>
2023-10-08 09:58:32 +0800

    update welcome msg

--- teamsBot.ts
+++ teamsBot.ts
@@ -35,9 +35,7 @@ export class TeamsBot extends TeamsActivityHandler {
       // Trigger command by IM text
       switch (txt) {
         case "welcome": {
-          const card = AdaptiveCards.declareWithoutData(rawWelcomeCard).render();
-          await context.sendActivity({ attachments: [CardFactory.adaptiveCard(card)] });
-          break;
+          await context.sendActivity("welcome!!");
         }
         case "learn": {
           this.likeCountObj.likeCount = 0;
