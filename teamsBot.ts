8d3d566dd095ac69868cb661e379aad202c324e0
yukun-dong <dongyukun1995@163.com>
2023-12-05 11:08:42 +0800

    update

--- teamsBot.ts
+++ teamsBot.ts
@@ -35,7 +35,7 @@ export class TeamsBot extends TeamsActivityHandler {
       // Trigger command by IM text
       switch (txt) {
         case "welcome": {
-          await context.sendActivity("welcome from azdo!!");
+          await context.sendActivity("welcome from azdo!! TEST 123123");
           break;
         }
         case "learn": {
